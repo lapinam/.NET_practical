67e5eb47f8b66c0b13a72799aeff61be9b0000f6
Marta Lapina <lapina.marta@gmail.com>
2022-11-11 18:40:20 +0200

    3rd commit

--- VSIntro/Program.cs
+++ VSIntro/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World on November the 9th!");
+Console.WriteLine("Hello, World on November the 9th!Hello extra world on November the 11th");
