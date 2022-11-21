7e36973d9f1d3a827e7d5c0f1dd9058847071062
Marta Lapina <lapina.marta@gmail.com>
2022-11-21 18:59:17 +0200

    Practical 2

---
+++ CalculatorExtra/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Enter number 1:");
+string input1 = Console.ReadLine();
+Console.WriteLine("Enter number 2:");
+string input2 = Console.ReadLine();
+Console.WriteLine("Choose the math operation:");
+Console.WriteLine("+ for addition, - for substraction, * for multiplication, / for division or % for modulo.");
+string input3 = Console.ReadLine();
+
+double number1 = double.Parse(input1);
+double number2 = double.Parse(input2);
+
+if (input3 == "+")
+{
+    double sum1 = number1 + number2;
+    Console.WriteLine(sum1);
+}
+else if (input3 == "-")
+{
+    double sub1 = number1 - number2;
+    Console.WriteLine(sub1);
+}
+else if (input3 == "*")
+{
+    double mult1 = number1 * number2;
+    Console.WriteLine(mult1);
+}
+else if (input3 == "/")
+{
+    double div1 = number1 / number2;
+    Console.WriteLine(div1);
+}
+else if (input3 == "%")
+{
+    double mod1 = number1 % number2;
+    Console.WriteLine(mod1);
+}
+
+if (number2 == 0 && input3 == "/")
+{
+    Console.WriteLine("The input is not valid!");
+}
---
+++ Zodiac/Program.cs
@@ -0,0 +1,19 @@
+﻿class Zodiac
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your birth month:");
+        int birthMonth = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Enter your birth day:");
+        int birthDay = Int32.Parse(Console.ReadLine());
+
+        if (birthMonth == 3 && birthDay >= 21)
+        {
+            Console.WriteLine("You're an Aries");
+        }
+        else if (birthMonth == 3 && birthDay <=21)
+        {
+            Console.WriteLine("You're a Pisces");
+        }
+    }
+}
