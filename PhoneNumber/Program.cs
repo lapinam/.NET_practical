1a1d6f63d570b5f1a5777309083a30566ac58342
Marta Lapina <lapina.marta@gmail.com>
2022-12-07 16:24:48 +0200

    phone nimber

---
+++ PhoneNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+class Lesson05122022
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your phone number");
+        string phoneNumber = Console.ReadLine();
+        
+        if (Regex.Match(phoneNumber, @"^(\+371){0,1}( ){0,1}[0-9]{8}$").Success)
+           Console.WriteLine("Thank you!");
+        else
+            Console.WriteLine("This number is invalid.");
+
+    }
+
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿class StringsTask
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter all the numbers (separated by space):");
+        string array = Console.ReadLine();
+        string[] numbersString = array.Split(" ");
+        int arraySize = array.Length;
+        int[] numbersInteger = new int[arraySize];
+        int i = 0;
+
+        for (i = 0; i < array.Length; i++)
+        {
+            numbersInteger[i] = Int32.Parse(numbersString[i]);
+           
+        }
+
+        
+
+        Console.WriteLine(numbersInteger);
+        //convert the array of string to array of integers
+        
+
+    }
+}
